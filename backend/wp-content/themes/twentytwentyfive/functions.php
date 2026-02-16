bda99866b2f3dc68cf91d18053a336483867b46a
Markus Mikely <markusmikely@gmail.com>
2026-02-16 19:24:12 +0000

    Removed default wordpress file from repo and added deployment action

--- backend/wp-content/themes/twentytwentyfive/functions.php
+++ backend/wp-content/themes/twentytwentyfive/functions.php
@@ -164,14 +164,3 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 		}
 	}
 endif;
-
-
-add_filter('acf/settings/save_json', function ($path) {
-    return get_stylesheet_directory() . '/acf-json';
-});
-
-add_filter('acf/settings/load_json', function ($paths) {
-    unset($paths[0]);
-    $paths[] = get_stylesheet_directory() . '/acf-json';
-    return $paths;
-});
