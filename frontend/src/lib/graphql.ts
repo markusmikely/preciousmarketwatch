d6b3ab761c606cf90fcf658214d51c3b9b074b69
Markus Mikely <markusmikely@gmail.com>
2026-02-22 22:51:04 +0000

    Point backend pipeline to correct dir

--- frontend/src/lib/graphql.ts
+++ frontend/src/lib/graphql.ts
@@ -2,6 +2,7 @@ import { GraphQLClient } from 'graphql-request';
 
 const endpoint = import.meta.env.VITE_WORDPRESS_API_URL || 'http://localhost:8888/wp/graphql';
 
+
 export const client = new GraphQLClient(endpoint, {
   headers: {
     'Content-Type': 'application/json',
