4f5a9fa60e423c3089ae6b66a8cd1fbbdce8f519
Markus J Mikely <markusmikely@gmail.com>
2026-03-12 16:42:47 +0000

    Resolved issue with migration links

--- pmw-agents/agents/db/migrations/versions/005b_stage_unique_constraint.py
+++ pmw-agents/agents/db/migrations/versions/005b_stage_unique_constraint.py
@@ -5,10 +5,10 @@ Revises: 005
 """
 from alembic import op
 
-revision = '005b'
-down_revision = '005'
-branch_labels = None
-depends_on = None
+revision = '005b_stage_unique_constraint'
+down_revision: Union[str, Sequence[str], None] = "005_observability"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_unique_constraint(
